b030c822eae9cd784298f2ba60ccb341eb0a3226
Yang Yu <yangyu@MacBook-Pro-3.local>
2017-09-29 14:57:27 +0800

    init

---
+++ src/main/java/com/tw/api/AppConfig.java
@@ -0,0 +1,12 @@
+package com.tw.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppConfig {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AppConfig.class, args);
+    }
+}
